aefa6e376bd2b2663efb0e08f0a6653f0fe0f03c
qy41tewa <murali.k.hemanna@fau.de>
2022-05-30 13:29:15 +0200

    Script to build docker and complete setup
    co-authored-by: khaiphamhoang.pi@gmail.com
    Signed-off-by: murali-kri5hna <murali.k.hemanna@fau.de>

---
+++ indy-node-monitor/fetch-validator-status/setup_script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+function prom_data_loop() {
+  counter=$1
+  while [[ ${counter} > 0 ]]
+  do
+    counter=$(( ${counter} - 1 ))
+    ./fetch_status_prometheus.py --genesis-url=http://172.17.0.1:9000/genesis --seed=000000000000000000000000Trustee1 -p | tee ../../data/prometheus/node_data.prom
+    sleep 60
+  done
+}
+
+cd ../../..
+
+## building von-network docker container
+cd von-network/
+./manage build
+./manage start
+read -p "how many samples: " SAMPLES
+
+wait
+sleep 15
+
+## building grafana and prometheus docker container
+cd ../amos2022ss06*/
+docker-compose up &
+
+## open the node exporter in another terminal for easy exit when necessary
+cd indy-node-monitor/node_exp*
+gnome-terminal -- node_exporter --collector.disable-defaults --collector.textfile.directory="../../data/prometheus/" --collector.textfile
+
+firefox http://172.17.0.1:3000 http://172.17.0.1:9090/targets http://172.17.0.1:9100/metrics
+
+
+## fetch the node metrics into the .prom file
+cd ../fetch-validator-status/
+prom_data_loop ${SAMPLES}
+#./fetch_status_prometheus.py --genesis-url=http://172.17.0.1:9000/genesis --seed=000000000000000000000000Trustee1 -p | tee ../../data/prometheus/node_data.prom
+
+
